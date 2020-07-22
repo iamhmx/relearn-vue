e391ea2b939abdbc76998ff7335bc26867b0e97b
helloyellow <helloyellow@aliyun.com>
2020-07-22 15:38:31 +0800

    add github link

--- src/components/Layout/title.vue
+++ src/components/Layout/title.vue
@@ -3,22 +3,40 @@
  * @Date: 2020-07-09 14:05:29
 --> 
 <template>
-  <div class="title">
-    {{$route.meta.title}}
+  <div class="rl-title-container">
+    <span class="title">{{$route.meta.title}}</span>
+    <div class="github"
+         @click="openGithub">
+      <svg-icon icon="github"></svg-icon>
+      Fork me on Github
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  created() {
-    console.log('route title：', this.$route)
+  methods: {
+    openGithub() {
+      window.open('https://github.com/iamhmx/relearn-vue', '_blank')
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.title {
-  text-align: left;
-  font-weight: bolder;
+.rl-title-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  justify-items: center;
+  .title {
+    text-align: left;
+    font-weight: bolder;
+  }
+  .github {
+    cursor: pointer;
+    font-weight: bolder;
+    font-style: italic;
+  }
 }
 </style>
